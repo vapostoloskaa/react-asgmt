f2e3c15b192e24d6ba707b84baf9cd1d10779996
vapostoloskaa <112665697+vapostoloskaa@users.noreply.github.com>
2022-09-02 13:37:06 +0200

    Update queries.js

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,8 +1,8 @@
 import gql from "graphql-tag";
 export const GET_SPACE_MISSION = gql`
 
-query GetSpaceMission($limit: Int!){
-    launchesPast($limit: $limit){
+query GetSpaceMission(limit: Int!){
+    launchesPast(limit: limit){
         mission_name
         launch_site{
             site_name_long
