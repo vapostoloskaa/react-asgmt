ac8236fa926b8caebf62278ad76b0913b2960b1b
vapostoloskaa <112665697+vapostoloskaa@users.noreply.github.com>
2022-09-06 13:36:32 +0200

    Update queries.js

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,17 +1,33 @@
 import gql from "graphql-tag";
 export const GET_SPACE_MISSION = gql`
 
-query GetSpaceMission($limit: Int!){
-    launchesPast(limit: $limit){
+    query GetSpaceMission($limit: Int!){
+    launchesPast(limit: $limit) {
         mission_name
-        launch_site{
-            site_name_long
+        launch_date_local
+        launch_site {
+          site_name_long
         }
-        rocket{
-            rocket_name
+        links {
+          article_link
+          video_link
         }
-        ships{
-           image 
+        rocket {
+          rocket_name
+          first_stage {
+            cores {
+              flight
+              core {
+                reuse_count
+            
+              }
+            }
+          }
+          
         }
+        ships {
+          image
+        }
+      }
     }
-}`;
+`;
