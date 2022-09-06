125300cfc13349caab634c0646b2323227d9812d
vapostoloskaa <112665697+vapostoloskaa@users.noreply.github.com>
2022-09-06 18:32:05 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
@@ -7,15 +8,16 @@ import reportWebVitals from './reportWebVitals';
 import { ApolloProvider } from '@apollo/client/react';
 import { apolloClient } from './graphql';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode client={apolloClient}>
-    <ApolloProvider>
-    <App />
-    </ApolloProvider>
-  </React.StrictMode>
-);
+ReactDOM.render(
+  <React.StrictMode>
+      <ApolloProvider client={apolloClient}>
+          <App />
+      </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 
+
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
